fd9220fbf0202f00723bde020f241af7220ba48b
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-11-01 16:20:59 +0500

    Version 0.10

---
+++ plugs/__init__.py
@@ -0,0 +1,7 @@
+from .table import Book
+from .sessions import Getter
+
+__ALL__ = (
+    Book,
+    Getter
+)
--- plugs/table.py
+++ plugs/table.py
@@ -10,8 +10,11 @@ class Book:
     def save_xlsx(self, data:list[list[str, int]]) -> str:
         self._sheet['A1'] = 'mac-адрес'
         self._sheet['B1'] = 'время использования (секунд)'
-        for num, item in enumerate(data):
-            pass
+        for num, item in enumerate(data, start=2):
+            self._sheet[f'A{num}'] = item[0]
+            self._sheet[f'B{num}'] = item[1]
+
+        return self._filename
 
     def save(self):
         self._book.save(self._filename)
