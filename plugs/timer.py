870403d27c5d777f7bac88949b69afbef48d2562
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-11-02 16:05:16 +0500

    Version 0.15

--- plugs/timer.py
+++ plugs/timer.py
@@ -1,6 +1,6 @@
 from time import time, gmtime,\
     strptime, sleep
-from threading import Thread
+from multiprocessing import Process
 from typing import Callable
 
 class Timer:
@@ -8,6 +8,7 @@ class Timer:
     def __init__(self, gap:int=28800, queue:list[Callable]=[]):
         self._gap = gap
         self._queue = queue
+        self._queue_thread:list[Process] = []
         
     def add_to_queue(self, func:Callable=None) -> None:
         if func is None:
@@ -18,4 +19,21 @@ class Timer:
         if index is None:
             raise ValueError
         del self._queue[index]
-        
+
+    def _start(self) -> None:
+        for func in self._queue:
+            self._queue_thread.append(Process(target=func))
+
+        for th in self._queue_thread:
+            th.start()
+
+    def _restart(self) -> None:
+        for th in self._queue_thread:
+            th.terminate()
+
+    def start_job(self) -> bool:
+        if all(map(callable, self._queue.copy())):
+            pr = Process(target=self._start())
+            pr.start()
+            return True
+        return False
