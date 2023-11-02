0f44b43c977e51757e280b0aa4927039b9479846
Yakov Savelev (Phone) <g6h6m238929@gmail.com>
2023-11-02 08:08:05 +0500

    Version 0.12

--- plugs/timer.py
+++ plugs/timer.py
@@ -1,8 +1,21 @@
 from time import time, gmtime,\
     strptime, sleep
 from threading import Thread
+from typing import Callable
 
 class Timer:
 
-    def __init__(self, gap:int=28800):
+    def __init__(self, gap:int=28800, queue:list[Callable]=[]):
         self._gap = gap
+        self._queue = queue
+        
+    def add_to_queue(self, func:Callable=None) -> None:
+        if func is None:
+            raise Exception("Function is not callable")
+        self._queue.append(func)
+        
+    def remove_from_queue(self, index:int=None) -> None:
+        if index is None:
+            raise ValueError
+        del self._queue[index]
+        
